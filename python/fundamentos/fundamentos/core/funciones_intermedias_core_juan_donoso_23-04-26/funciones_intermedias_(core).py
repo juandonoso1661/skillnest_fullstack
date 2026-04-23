6402b763ec784e0461f4550ee66f1d4cef7679a7
juandonoso1661 <juandonoso@liceovvh.cl>
2026-04-23 13:58:03 -0400

    avanze

---
+++ python/fundamentos/fundamentos/core/funciones_intermedias_core_juan_donoso_23-04-26/funciones_intermedias_(core).py
@@ -0,0 +1,27 @@
+# 1. Cambiar el puntaje de Pedro a 75
+datos = [
+   {"nombre": "Carlos", "puntaje": 80},
+   {"nombre": "María", "puntaje": 95},
+   {"nombre": "Pedro", "puntaje": 75}
+]
+
+print(datos[2]["nombre"])
+datos[2]["puntaje"] = 75
+print(datos[2]["puntaje"])
+
+# 2. Crear función que imprima:
+#    "Carlos obtuvo 80 puntos"
+def datos_carlos():
+    for i in datos:
+        print(f"{datos[0]['nombre']} obtuvo {datos[0]['puntaje']} puntos")
+        break
+datos_carlos()
+
+# 3. Crear función que reciba "nombre" o "puntaje" e imprima solo esos valores
+
+def recibir_datos(datos):
+   ingresarNombre = input("Ingrese un nombre: ")
+   for i in datos:
+        if ingresarNombre == datos["nombre"]:
+            print(f"{'nombre'} obtuvo {'puntaje'} puntos")
+recibir_datos(datos)
