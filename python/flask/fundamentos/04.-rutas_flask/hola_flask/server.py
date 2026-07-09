e3a038d2d6966c596118a53f4eb678ec5da5b95b
juandonoso1661 <juandonoso@liceovvh.cl>
2026-07-09 15:15:31 -0400

    fesfs

---
+++ python/flask/fundamentos/04.-rutas_flask/hola_flask/server.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hola_mundo():
+    return "¡Hola Mundo!"      
+
+@app.route("/exito")
+def exito():
+    return "¡Éxito!"
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
