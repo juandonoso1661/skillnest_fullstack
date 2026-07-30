b7cabe008543d7a25ba1eabd831a03a40c6dffac
juandonoso1661 <juandonoso@liceovvh.cl>
2026-07-30 15:15:14 -0400

    trhytrhtrhtr

--- python/core/visualizacion_tablas_juan_donoso_2026-07-30/app.py
+++ python/core/visualizacion_tablas_juan_donoso_2026-07-30/app.py
@@ -4,19 +4,19 @@ app = Flask(__name__)
 
 # Base de datos ficticia de plataformas digitales
 datos = [
-   {"nombre": "Spotify", "usuarios": "515M", "fundado": "2006", "pais": "Suecia"},
-   {"nombre": "Netflix", "usuarios": "247M", "fundado": "1997", "pais": "EE.UU."},
-   {"nombre": "YouTube", "usuarios": "2.5B", "fundado": "2005", "pais": "EE.UU."},
-   {"nombre": "Twitch", "usuarios": "140M", "fundado": "2011", "pais": "EE.UU."},
-   {"nombre": "TikTok", "usuarios": "1.7B", "fundado": "2016", "pais": "China"},
-   {"nombre": "Instagram", "usuarios": "2.35B", "fundado": "2010", "pais": "EE.UU."},
-   {"nombre": "Discord", "usuarios": "250M", "fundado": "2015", "pais": "EE.UU."},
+   {"nombre": "Spotify", "usuarios": "515M", "fundado": "2006", "pais": "Suecia", "icono":"spotify.png"},
+   {"nombre": "Netflix", "usuarios": "247M", "fundado": "1997", "pais": "EE.UU.", "icono":"netflix.png"},
+   {"nombre": "YouTube", "usuarios": "2.5B", "fundado": "2005", "pais": "EE.UU.", "icono":"youtube.png"},
+   {"nombre": "Twitch", "usuarios": "140M", "fundado": "2011", "pais": "EE.UU.", "icono":"twich.png"},
+   {"nombre": "TikTok", "usuarios": "1.7B", "fundado": "2016", "pais": "China", "icono":"tiktok.png"},
+   {"nombre": "Instagram", "usuarios": "2.35B", "fundado": "2010", "pais": "EE.UU.", "icono":"instagram.png"},
+   {"nombre": "Discord", "usuarios": "250M", "fundado": "2015", "pais": "EE.UU.", "icono":"discord.png"},
 ]
 
 # Ruta para mostrar la tabla con datos
 @app.route("/")
 def tablas_plataformas():
-   return render_template("tablas.html", datos=datos)
+   return render_template("tablas.html", plataformas=datos)
 
 if __name__ == "__main__":
    app.run(debug=True)
